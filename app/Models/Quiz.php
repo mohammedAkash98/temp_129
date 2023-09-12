1321835c13322b331babe92d0e5a692357080cb6
Saiful Islam Shaon <shasanexp@gmail.com>
2023-09-12 18:42:37 +0600

    chapter lesson quiz table done

---
+++ app/Models/Quiz.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Quiz extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+
+
+    public function chapter()
+    {
+        return $this->belongsTo(Chapter::class);
+    }
+
+    public function lesson()
+    {
+        return $this->belongsTo(Lesson::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
