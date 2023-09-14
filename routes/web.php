e67981be5c7b7b9c4d15f6f9e4e71ab69b1b2e32
tawshiqulislam <tawshiq.rafi02@gmail.com>
2023-09-14 09:47:40 +0600

    .

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [DashboardController::class, 'home'])->name('home');
+Route::get('/', [FrontendController::class, 'home'])->name('home');
 // Route::get('/', [DashboardController::class, 'dashboard'])->name('dashboard')->middleware('auth');
 Route::get('/admin', [BackendController::class, 'admin'])->name('admin');
 
