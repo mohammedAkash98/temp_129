fe7ba9a146001edfe2525f860272e936055e816e
shaonlimit <shasanexp@gmail.com>
2023-09-19 13:29:31 +0600

    certificate updated

--- app/Http/Controllers/ChapterController.php
+++ app/Http/Controllers/ChapterController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Chapter;
 use App\Models\Lesson;
 use App\Models\Overview;
-use App\Models\Result;
 use Illuminate\Http\Request;
 
 class ChapterController extends Controller
@@ -28,20 +27,6 @@ class ChapterController extends Controller
 
     public function courses_name($id)
     {
-        $lesson_length = Lesson::all()->count();
-
-        $user = auth()->user();
-        $current_lesson_id = auth()->user()->overview->current_lesson_id;
-        $current_chapter_id = auth()->user()->overview->current_chapter_id;
-
-        $result = Result::where('user_id', $user->id)
-            ->where('lesson_id', $current_lesson_id)
-            ->where('chapter_id', $current_chapter_id)
-            ->first();
-
-        if ($result && $result->lesson_id && $result->lesson_id == $lesson_length) {
-            return view('frontend.certificate', compact('user'));
-        }
 
         $chapters = Chapter::all();
         // return $chapters;
--- storage/framework/views/142167e3559d529c3cac5fd041c7071e.php
+++ storage/framework/views/142167e3559d529c3cac5fd041c7071e.php
@@ -3,8 +3,26 @@
     <div class="container">
         <div class="row">
             <div class="col-md-6 mx-auto text-center mt-5">
-                <h2 class="mb-4">দুঃখিত আপনি ইতোমধ্যেই এই কুইজে অংশগ্রহণ করেছেন ।</h2>
-                <a href="<?php echo e(route('courses')); ?>" class="btn btn-success ">কোর্সে ফিরে যান</a>
+                <?php
+                    $lesson_length = \App\Models\Lesson::all()->count();
+
+                    $user = auth()->user();
+                    $current_lesson_id = auth()->user()->overview->current_lesson_id;
+                    $current_chapter_id = auth()->user()->overview->current_chapter_id;
+
+                    $result = \App\Models\Result::where('user_id', $user->id)
+                        ->where('lesson_id', $current_lesson_id)
+                        ->where('chapter_id', $current_chapter_id)
+                        ->first();
+
+                    if ($result && $result->lesson_id && $result->lesson_id == $lesson_length) {
+                        echo '<h2 class="mb-4">আপনি সফলভাবে সকল অধ্যায় সম্পন্ন করেছেন।</h2>';
+                        echo '<a href="'.route('certificate').'" class="btn btn-success">সার্টিফিকেট ডাউনলোড করুন</a>';
+                    } else {
+                        echo '<h2 class="mb-4">দুঃখিত আপনি ইতোমধ্যেই এই কুইজে অংশগ্রহণ করেছেন ।</h2>';
+                        echo '<a href="'.route('courses').'" class="btn btn-success">কোর্সে ফিরে যান</a>';
+                    }
+                ?>
             </div>
         </div>
     </div>
