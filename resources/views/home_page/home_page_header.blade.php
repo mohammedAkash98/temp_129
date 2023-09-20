b8b5f2b1828946cf5736b646cca5ef072a7758a2
rubayamim381 <mimjkkniu.381@gmail.com>
2023-09-20 15:28:53 +0600

    home_page_design

---
+++ resources/views/home_page/home_page_header.blade.php
@@ -0,0 +1,75 @@
+<nav class="navbar navbar-expand-lg bg-white navbar_top">
+
+    <div class="container-fluid">
+
+        <a class="navbar-brand" href="{{ Route('home_page') }}">
+            <img src="{{ asset('assets/img/Screenshot_9_1.png') }}" alt="Logo" width="100">
+        </a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mx-auto mb-2 mb-lg-0 align-items-center">
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="#!">
+                        <img class="img-fluid" src="{{ asset('assets/icon/house.png') }}" alt="">
+                        হোম</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="#!">
+                        <img class="img-fluid" src="{{ asset('assets/icon/course.png') }}" alt="">
+                        কোর্স সমূহ</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="#!">
+                        <img class="img-fluid" src="{{ asset('assets/icon/blogging.png') }}" alt="">
+                        ব্লগ</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="#!">
+                        <img class="img-fluid" src="{{ asset('assets/icon/webinar.png') }}" alt="">
+                        ওয়েবিনার</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="#!">
+                        <img class="img-fluid" src="{{ asset('assets/icon/help.png') }}" alt="">
+                        সহায়িকা</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="#!">
+                        <img class="img-fluid" src="{{ asset('assets/icon/teamwork.png') }}" alt="">
+                        আমাদের সম্পর্কে</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="#!">
+                        <img class="img-fluid" src="{{ asset('assets/icon/contact.png') }}" alt="">
+                        যোগাযোগ</a>
+                </li>
+                <li class="nav-item">
+                    <div class="btn-group">
+                        <button class="btn btn-sm dropdown-toggle" type="button"
+                            data-bs-toggle="dropdown" data-bs-auto-close="inside" aria-expanded="false">
+                            <img class="img-fluid" src="{{ asset('assets/icon/languages.png') }}" alt="">
+
+                            ভাষা
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">বাংলা</a></li>
+                            <li><a class="dropdown-item" href="#">ইংরেজী</a></li>
+                        </ul>
+                    </div>
+                </li>
+
+            </ul>
+            <div class="text-center">
+                <button type="button" class="btn btn-color">Login / Register</button>
+            </div>
+        </div>
+    </div>
+    <div class="navbar-bottom-img">
+        <img src="{{asset("assets/icon/nav-bg.png")}}" class="position-absolute w-100" alt="">
+    </div>
+</nav>
---
+++ resources/views/home_page/home_page_script_cdn.blade.php
@@ -0,0 +1,15 @@
+{{-- <script src="{{ asset('ui/backend') }}/assets/vendor/apexcharts/apexcharts.min.js"></script>
+<script src="{{ asset('ui/backend') }}/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="{{ asset('ui/backend') }}/assets/vendor/chart.js/chart.umd.js"></script>
+<script src="{{ asset('ui/backend') }}/assets/vendor/echarts/echarts.min.js"></script>
+<script src="{{ asset('ui/backend') }}/assets/vendor/quill/quill.min.js"></script>
+<script src="{{ asset('ui/backend') }}/assets/vendor/simple-datatables/simple-datatables.js"></script>
+<script src="{{ asset('ui/backend') }}/assets/vendor/tinymce/tinymce.min.js"></script>
+<script src="{{ asset('ui/backend') }}/assets/vendor/php-email-form/validate.js"></script> --}}
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+    integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous">
+</script>
