91e940b513dc40e5968eb65c9540ddd355e95565
shaonlimit <shasanexp@gmail.com>
2023-09-20 16:31:02 +0600

    update ui and backend

--- resources/views/frontend/user/register.blade.php
+++ resources/views/frontend/user/register.blade.php
@@ -2,7 +2,7 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href=" https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 @section('page_content')
-{{-- registration --}}
+
     <section class="registrationPage mb-4">
         <div class="container mt-5">
             <div class="container-fluid">
